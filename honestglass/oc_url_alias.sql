c54af078b414d0b4b87b0251c73ea1dec9a01746
diego <spositod@gmail.com>
2015-11-19 20:20:28 -0300

    add information urls alias

---
+++ honestglass/oc_url_alias.sql
@@ -0,0 +1,5 @@
+DELETE FROM oc_url_alias WHERE query like "%information%";
+INSERT INTO oc_url_alias VALUES (NULL,'information_id=3','privacy');
+INSERT INTO oc_url_alias VALUES (NULL,'information_id=4','about-us');
+INSERT INTO oc_url_alias VALUES (NULL,'information_id=5','terms');
+INSERT INTO oc_url_alias VALUES (NULL,'information_id=7','help');
