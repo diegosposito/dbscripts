ff5abebb7f22434658562ebd2dd1c29e00aef7b3
diego <spositod@gmail.com>
2015-12-14 14:41:04 -0300

    cambiar grupo a sellers

--- honestglass/add_sellers.sql
+++ honestglass/add_sellers.sql
@@ -1,6 +1,6 @@
 # AGREGAR VENDEDOR 1
 
-INSERT INTO oc_customer SET customer_group_id = '1', store_id = '0', firstname = 'seller1', lastname = 'seller1', email = 'seller1@gmail.com', telephone = '', fax = '', custom_field = '', salt = '077ff0a87', password = '08ee8ec1a88cbe8db4457767eb8c7cdb5eb86b4e', newsletter = '0', ip = '127.0.0.1', status = '1', approved = '1', date_added = NOW();
+INSERT INTO oc_customer SET customer_group_id = '2', store_id = '0', firstname = 'seller1', lastname = 'seller1', email = 'seller1@gmail.com', telephone = '', fax = '', custom_field = '', salt = '077ff0a87', password = '08ee8ec1a88cbe8db4457767eb8c7cdb5eb86b4e', newsletter = '0', ip = '127.0.0.1', status = '1', approved = '1', date_added = NOW();
 
 INSERT INTO oc_address SET customer_id = '1', firstname = 'seller1', lastname = 'seller1', company = '', address_1 = '', address_2 = '', city = '', postcode = '', country_id = '0', zone_id = '0', custom_field = '';
 
@@ -41,7 +41,7 @@ UPDATE oc_ms_seller
 
 #AGREGAR VENDEDOR 2
 
- INSERT INTO oc_customer SET customer_group_id = '1', store_id = '0', firstname = 'seller2', lastname = 'seller2', email = 'seller2@gmail.com', telephone = '', fax = '', custom_field = '', salt = '77ddffe7c', password = 'bdc0ee8016b0c879e9d3500ff86be83e65983ed8', newsletter = '0', ip = '127.0.0.1', status = '1', approved = '1', date_added = NOW();
+ INSERT INTO oc_customer SET customer_group_id = '2', store_id = '0', firstname = 'seller2', lastname = 'seller2', email = 'seller2@gmail.com', telephone = '', fax = '', custom_field = '', salt = '77ddffe7c', password = 'bdc0ee8016b0c879e9d3500ff86be83e65983ed8', newsletter = '0', ip = '127.0.0.1', status = '1', approved = '1', date_added = NOW();
 
 INSERT INTO oc_address SET customer_id = '2', firstname = 'seller2', lastname = 'seller2', company = '', address_1 = '', address_2 = '', city = '', postcode = '', country_id = '0', zone_id = '0', custom_field = '';
 
