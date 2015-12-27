250039cceed3297361a18628826682e5906f1782
diego <spositod@gmail.com>
2015-12-27 11:08:02 -0300

    buyer1 now have username

--- honestglass/add_sellers.sql
+++ honestglass/add_sellers.sql
@@ -41,7 +41,7 @@ UPDATE oc_ms_seller
 
 #AGREGAR VENDEDOR 2
 
- INSERT INTO oc_customer SET customer_group_id = '2', store_id = '0', firstname = 'seller2', lastname = 'seller2', email = 'seller2@gmail.com', telephone = '', fax = '', custom_field = '', salt = '77ddffe7c', password = 'bdc0ee8016b0c879e9d3500ff86be83e65983ed8', newsletter = '0', ip = '127.0.0.1', status = '1', approved = '1', date_added = NOW();
+INSERT INTO oc_customer SET customer_group_id = '2', store_id = '0', firstname = 'seller2', lastname = 'seller2', email = 'seller2@gmail.com', telephone = '', fax = '', custom_field = '', salt = '77ddffe7c', password = 'bdc0ee8016b0c879e9d3500ff86be83e65983ed8', newsletter = '0', ip = '127.0.0.1', status = '1', approved = '1', date_added = NOW();
 
 INSERT INTO oc_address SET customer_id = '2', firstname = 'seller2', lastname = 'seller2', company = '', address_1 = '', address_2 = '', city = '', postcode = '', country_id = '0', zone_id = '0', custom_field = '';
 
@@ -83,7 +83,7 @@ UPDATE oc_ms_seller
 
 
 # AGREGAR BUYER 1
-INSERT INTO oc_customer SET customer_group_id = '1', store_id = '0', firstname = 'buyer1', lastname = 'buyer1', email = 'buyer1@gmail.com', telephone = '123123123', fax = '123', custom_field = '', salt = '365591f3b', password ='9a048dfde3b0c8df7a79ba2f8d9c4e594a70aabc', newsletter = '0', ip = '127.0.0.1', status = '1', approved = '1', date_added = NOW();
+INSERT INTO oc_customer SET customer_group_id = '1', store_id = '0', firstname = 'buyer1', lastname = 'buyer1', email = 'buyer1@gmail.com', telephone = '123123123', fax = '123', custom_field = '', salt = '365591f3b', password ='9a048dfde3b0c8df7a79ba2f8d9c4e594a70aabc', newsletter = '0', ip = '127.0.0.1', status = '1', approved = '1', username = 'buyer1', date_added = NOW();
 
 INSERT INTO oc_address SET customer_id = '3', firstname = 'buyer1', lastname = 'buyer1', company = 'company1', address_1 = 'Quebec 123', address_2 = '', city = 'Quebec', postcode = '456', country_id = '38', zone_id = '612', custom_field = '';
 
