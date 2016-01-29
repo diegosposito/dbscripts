0e87206f8b078ea84fd5744789c521bb1f9067fd
diegosposito <spositod@gmail.com>
2016-01-29 11:34:45 -0300

    update csript

--- honestglass/script.sh
+++ honestglass/script.sh
@@ -17,7 +17,7 @@ mysql -uroot -proot911 $1 < add_filters.sql
 
 mysql -uroot -proot911 $1 < add_relations_categories_filters.sql
 
-mysql -uroot -proot911 $1 < filtros-category.sql
+#mysql -uroot -proot911 $1 < filtros-category.sql
 
 mysql -uroot -proot911 $1 < oc_setting.sql
 
