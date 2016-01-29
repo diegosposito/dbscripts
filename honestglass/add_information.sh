2bb2d63f78831cbf0dd4c967c15e0843036e9c4b
diegosposito <spositod@gmail.com>
2016-01-29 11:35:07 -0300

    change script

--- honestglass/add_information.sh
+++ honestglass/add_information.sh
@@ -1,6 +1,6 @@
-mysql -uroot -proot911 $1 < add_sellers.sql
+#mysql -uroot -proot911 $1 < add_sellers.sql
 
-mysql -uroot -proot911 $1 < add_products.sql
+#mysql -uroot -proot911 $1 < add_products.sql
 
 mysql -uroot -proot911 $1 < oc_url_alias.sql
 
