4177802731f59a3a87423820ec7c3de7c7f1534d
diego <spositod@gmail.com>
2016-01-30 08:38:27 -0300

    add index on table oc_product_description

--- honestglass/production/01-29-16_modify_products_table.sql
+++ honestglass/production/01-29-16_modify_products_table.sql
@@ -1,3 +1,6 @@
 
 ALTER TABLE oc_product_description MODIFY COLUMN description VARCHAR(255) NOT NULL;
 ALTER TABLE oc_product_description MODIFY COLUMN tag VARCHAR(255) NOT NULL;
+
+CREATE INDEX idx_tag ON oc_product_description(tag) using BTREE;
+CREATE INDEX idx_description ON oc_product_description(description) using BTREE;
